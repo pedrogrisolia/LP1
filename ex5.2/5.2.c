c645c8b4f9f4a90c571e4977e7bdd343490150ed
pedromg19 <43138917+pedromg19@users.noreply.github.com>
2018-09-26 14:22:22 -0300

    Add files via upload

---
+++ ex5.2/5.2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(void) {
+	int conta1;
+	int conta2;
+	int valor=500;
+	int* conta;
+	scanf("%d %d",&conta1,&conta2);
+	if(conta1>conta2) {
+		conta=&conta1;
+	}
+	else {
+		conta=&conta2;
+	}
+	compra(conta,valor);
+	printf("%d %d\n",conta1,conta2);
+	return 0;
+}
+int compra(int* conta, int valor) {
+	*conta=*conta-500;
+	return 0;
+}	
+	
+	
