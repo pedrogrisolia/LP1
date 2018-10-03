ff164d2881a267c5b4a1627969775624128dfe2d
pedromg19 <43138917+pedromg19@users.noreply.github.com>
2018-10-03 12:57:39 -0300

    Add files via upload

---
+++ ex6.1/6.1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+void preenche(int n, int* vec) {
+	int i;
+	for(i=0;i<n;i++) {
+		scanf("%d",&vec[i]);
+	}
+}
+float media(int n, int* vec) {
+	int soma,x;
+	float med;
+	for(x=0;x<n;x++) {
+		soma=soma+vec[x];
+	}
+	med=soma/n;
+	printf("Média: %f\n",med);
+	return 0;
+}
+int main(void) {
+	int n;
+	int vec[1000];
+	printf("Digite N: ");
+	scanf("%d",&n);
+	preenche(n,vec);
+	media(n,vec);
+	return 0;
+}
