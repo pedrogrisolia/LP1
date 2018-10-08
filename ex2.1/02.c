9866c8f1970cf5c0df1f8224b85e770694e270d2
pedromg19 <43138917+pedromg19@users.noreply.github.com>
2018-10-08 15:37:44 -0300

    Update 02.c

--- ex2.1/02.c
+++ ex2.1/02.c
@@ -10,12 +10,15 @@ int main (void) {
 	if (num>num2) {
 		printf("%d é maior que %d \n", num,num2);
 	} 
-	if (num==num2) {
-		printf("%d é igual a %d \n",num,num2);
+	else {
+		if (num==num2) {
+			printf("%d é igual a %d \n",num,num2);
+		}
+		else {
+			printf("%d é menor que %d \n",num,num2);
+		}
 	}
-	if (num<num2) {
-		printf("%d é menor que %d \n",num,num2);
-	}
+		
 	return 0;
 }
 	
