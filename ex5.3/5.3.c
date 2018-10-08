57db687b34769a92e0db1fd6410cacd479bb143e
pedromg19 <43138917+pedromg19@users.noreply.github.com>
2018-10-08 09:52:36 -0300

    Update 5.3.c

--- ex5.3/5.3.c
+++ ex5.3/5.3.c
@@ -1,30 +1,23 @@
 #include <stdio.h>
-int compra(int* contama,int* contame,int compras[]) {
-	char i;
-	printf("\n%d %d\n",*contama,*contame);
-	for(i=0;i<5;i++) {
-		*contama=*contama-compras[i];
-		printf("%d %d\n",*contama,*contame);
-	}
-	return 0;
-}	
-	
+void compra(int* c, int valor) {
+	*c=*c-valor;
+} 
 int main(void) {
-	int conta1;
-	int conta2;
+	int conta1,conta2,v;
+	int* conta;
+	char i;
 	int compras[]={100,50,80,30,20};
-	int* contama;
-	int* contame;
 	scanf("%d %d",&conta1,&conta2);
-	if(conta1>conta2) {
-		contama=&conta1;
-		contame=&conta2;
+	for(i=0;i<5;i++) {
+		v=compras[i];
+		if(conta1>conta2) {
+			conta=&conta1;
+		}
+		else {
+			conta=&conta2;
+		}
+		compra(conta,v);
+		printf("%d %d\n",conta1,conta2);
 	}
-	else {
-		contama=&conta2;
-		contame=&conta1;
-	}
-	compra(contama,contame,compras);
 	return 0;
 }
-	
