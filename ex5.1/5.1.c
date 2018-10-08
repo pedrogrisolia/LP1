557eaa44220be070c84cffb692ba182c6ad28928
pedromg19 <43138917+pedromg19@users.noreply.github.com>
2018-10-08 09:24:49 -0300

    Update 5.1.c

--- ex5.1/5.1.c
+++ ex5.1/5.1.c
@@ -4,13 +4,12 @@ int main(void) {
 	scanf("%d %d",&a,&b);
 	printf("%d %d\n",a,b);
 	if(a>b) {
-		int* p=&a;
-		*p=*p-50;
+		int* p=&a;	
 	}
 	else {
 		int* p=&b;
-		*p=*p-50;
 	}
+	*p=*p-50;
 	printf("%d %d\n",a,b);
 	return 0;
 }
